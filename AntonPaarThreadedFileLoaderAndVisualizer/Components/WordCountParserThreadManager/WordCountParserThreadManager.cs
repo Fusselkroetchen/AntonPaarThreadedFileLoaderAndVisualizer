ebb6724cf8de5593a00a1091b976dd11336be391
Sebastian Schmidt <webmaster@geekonarium.de>
2025-02-27 12:46:53 +0100

    remove commends, more infos

--- AntonPaarThreadedFileLoaderAndVisualizer/Components/WordCountParserThreadManager/WordCountParserThreadManager.cs
+++ AntonPaarThreadedFileLoaderAndVisualizer/Components/WordCountParserThreadManager/WordCountParserThreadManager.cs
@@ -2,13 +2,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Threading;
-
-//using System.Collections.Generic;
-//using System.Diagnostics;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
 
