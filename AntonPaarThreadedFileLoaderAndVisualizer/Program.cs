56d999f103890b2aa8eda7c58865930b5e945e98
Sebastian Schmidt <webmaster@geekonarium.de>
2025-02-27 14:19:38 +0100

    Boost App

--- AntonPaarThreadedFileLoaderAndVisualizer/Program.cs
+++ AntonPaarThreadedFileLoaderAndVisualizer/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace AntonPaarThreadedFileLoaderAndVisualizer
 {
     internal static class Program
@@ -8,6 +10,9 @@ namespace AntonPaarThreadedFileLoaderAndVisualizer
         [STAThread]
         static void Main()
         {
+            System.Diagnostics.Process myProcess = System.Diagnostics.Process.GetCurrentProcess();
+            myProcess.PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
